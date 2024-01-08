75a98e5b9aa1278b814eee619299c59f1f9922bd
alelouis <me@alelouis.eu>
2024-01-08 20:40:30 +0100

    refactor render board

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+use graphics::{clear, rectangle, Transformed};
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use crate::board::Board;
@@ -11,30 +12,23 @@ pub struct App {
 
 impl App {
     pub fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
-
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-
-        let square_size = self.height as f32 / 8.0;
-
         let board = Board::new();
+        self.render_board(&board, args);
+    }
 
+    pub fn render_board(&mut self, board: &Board, args: &RenderArgs) {
         self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
+            // Clear the screen
+            clear([0.0, 1.0, 0.0, 1.0], gl);
 
+            let square_size = self.height as f32 / 8.0;
             for s in board.squares.iter() {
                 let (rank, file) = s.index_to_file_rank();
-                let square = rectangle::square(0.0, 0.0, square_size.into());
-
                 let (x, y) = (file as f32 * square_size, rank as f32 * square_size);
-
                 let transform = c
                     .transform
                     .trans(x.into(), y.into());
-
-                // Draw a box rotating around the middle of the screen.
-                rectangle(s.color, square, transform, gl);
+                rectangle(s.color, rectangle::square(0.0, 0.0, square_size.into()), transform, gl);
             }
         });
     }
